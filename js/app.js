5166cd61dd904846a6a4871fddc732f2fc5b47f0
Stephen L. Blum <stephen@pubnub.com>
2014-09-06 18:28:28 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -36,8 +36,9 @@
 	var channel = 'draw';
 
 	var pubnub = PUBNUB.init({
-		publish_key: 'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695',
-		subscribe_key: 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
+		publish_key     : 'pub-c-156a6d5f-22bd-4a13-848d-b5b4d4b36695',
+		subscribe_key   : 'sub-c-f762fb78-2724-11e4-a4df-02ee2ddab7fe',
+		leave_on_unload : true
 	});
 
 	pubnub.subscribe({
